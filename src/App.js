7da459acb28686d2ca71dbaf38f4fe6f8a42fad1
NK4539 <nupurkapoor04@gmail.com>
2024-03-03 10:14:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React,{useState} from "react";
 import './App.css';
 
+const data=[
+  {
+    name:"Gmail",
+    url: ["https://www.google.com"]
+  },
+  {
+    name:"Internshala",
+    url: ["https://internshala.com/"]
+  },
+  {
+    name:"Lever",
+    url: ["https://www.lever.co/?utm_referrer=https%3A%2F%2Fwww.google.com%2F"]
+  },
+  {
+    name:"Wellfound",
+    url: ["https://wellfound.com/"]
+  }
+]
+
+const openTab=(url)=>{
+  window.open(url,'_blank');
+
+}
+
 function App() {
+  const [lists, setLists] = useState(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 className="title">Rapport.AI</h3>
+      <div className="lists">
+        {lists && lists.map((item)=>{
+          return(
+            <button className="button" onClick={()=>{openTab(item.url)}}>{item.name}</button>
+          )
+        })}
+        
+      </div>
     </div>
   );
 }
